49bcc947d7b3d0a5b189fb402b610436ece33148
Koushik Maratha <koushik.maratha@gmail.com>
2021-01-10 00:27:26 +0530

    Completed Home Screen accoriding to the Mockup

---
+++ js/customScript.js
@@ -0,0 +1,14 @@
+$('document').ready(()=>{
+    $('#link1').click(()=>{
+        $('#link1').text('Buy 5%')
+    })
+    $('#link2').click(()=>{
+        $('#link2').text('Buy 5%')
+    })
+    $('#link3').click(()=>{
+        $('#link3').text('Buy 5%')
+    })
+    $('#link4').click(()=>{
+        $('#link4').text('Buy 5%')
+    })
+})
